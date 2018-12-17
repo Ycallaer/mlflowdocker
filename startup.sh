c56ed853a43570a946a1ed7203ba23b3489b6b1f
yvescallaert <yves.callaert@maersk.com>
2018-12-17 13:17:57 +0100

    adding file store hardcoded

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mlflow server --file-store "$MLFLOW_SERVER_FILE_STORE" --default-artifact-root "$MLFLOW_SERVER_DEFAULT_ARTIFACT_ROOT" --host 0.0.0.0
+mlflow server --file-store /tmp/mlflow --default-artifact-root "$MLFLOW_SERVER_DEFAULT_ARTIFACT_ROOT" --host 0.0.0.0
